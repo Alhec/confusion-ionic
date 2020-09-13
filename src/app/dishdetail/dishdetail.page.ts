5e34feddb6e2bb9c8caf1642caf7ba700c51f584
alhec <hector080896@gmail.com>
2020-09-13 12:48:50 -0400

    FAB Favorites

--- src/app/dishdetail/dishdetail.page.ts
+++ src/app/dishdetail/dishdetail.page.ts
@@ -2,8 +2,10 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { Comment } from '../shared/comment';
 import { DishService } from '../providers/dish.service';
+import { FavoriteService } from '../providers/favorite.service';
 import { Params, ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-dishdetail',
@@ -16,10 +18,13 @@ export class DishdetailPage implements OnInit {
   errMess: string;
   avgstars: string;
   numcomments: number;
+  favorite: boolean;
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    @Inject('BaseURL') private BaseURL) { }
+    @Inject('BaseURL') private BaseURL,
+    private favoriteservice: FavoriteService) {
+    }
 
   ngOnInit() {
     this.route.params.pipe(switchMap((params: Params) => { console.log(params);return this.dishservice.getDish(params['id']); }))
@@ -28,8 +33,14 @@ export class DishdetailPage implements OnInit {
           this.numcomments = this.dish.comments.length;
           let total = 0;
           this.dish.comments.forEach(comment => total += comment.rating );
-          this.avgstars = (total/this.numcomments).toFixed(2);  },
+          this.avgstars = (total/this.numcomments).toFixed(2);
+          this.favorite = this.favoriteservice.isFavorite(this.dish.id);
+        },
         errmess => this.errMess = <any>errmess);
   }
 
+  addToFavorites() {
+    console.log('Adding to Favorites', this.dish.id);
+    this.favorite = this.favoriteservice.addFavorite(this.dish.id);
+  }
 }
